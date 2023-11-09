114a99554d0c41c83240b26faf85c015bd0cc22a
강문주 <moon@secondspace.kr>
2023-11-09 15:30:15 +0900

    second commit

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("안녕~ 난 문쥬씨야!!!!");
+        System.out.println("안녕~ 난 문쥬얌");
     }
 }
